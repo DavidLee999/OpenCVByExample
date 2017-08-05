d1f1de54866339cdd7aa9e5ef0c0a491693f8655
DavidLee999 <MoonLee003@outlook.com>
2017-08-05 20:26:42 +0200

    update

--- Ch2/sample2.cpp
+++ Ch2/sample2.cpp
@@ -13,42 +13,42 @@ const char* keys =
 	"{@video | | Video file, if not defined try to use webcamera}"
 };
 
-//int main(int argc, const char** argv)
-//{
-//	CommandLineParser parser(argc, argv, keys);
-//	parser.about("Chapter 2. v1.0.0");
-//
-//	if (parser.has("help"))
-//	{
-//		parser.printMessage();
-//		return 0;
-//	}
-//	String videoFile = parser.get<String>(0);
-//
-//	if (!parser.check())
-//	{
-//		parser.printErrors();
-//		return 0;
-//	}
-//
-//	VideoCapture cap;
-//	if (videoFile != "")
-//		cap.open(videoFile);
-//	else
-//		cap.open(0);
-//	if (!cap.isOpened())
-//		return -1;
-//
-//	namedWindow("Video", 1);
-//	for (;;)
-//	{
-//		Mat frame;
-//		cap >> frame;
-//		imshow("Video", frame);
-//		if (waitKey(30) >= 0)
-//			break;
-//	}
-//	cap.release();
-//	
-//	return 0;
-//}
+int main(int argc, const char** argv)
+{
+    CommandLineParser parser(argc, argv, keys);
+    parser.about("Chapter 2. v1.0.0");
+
+    if (parser.has("help"))
+    {
+        parser.printMessage();
+        return 0;
+    }
+    String videoFile = parser.get<String>(0);
+
+    if (!parser.check())
+    {
+        parser.printErrors();
+        return 0;
+    }
+
+    VideoCapture cap;
+    if (videoFile != "")
+        cap.open(videoFile);
+    else
+        cap.open(0);
+    if (!cap.isOpened())
+        return -1;
+
+    namedWindow("Video", 1);
+    for (;;)
+    {
+        Mat frame;
+        cap >> frame;
+        imshow("Video", frame);
+        if (waitKey(30) >= 0)
+            break;
+    }
+    cap.release();
+
+    return 0;
+}

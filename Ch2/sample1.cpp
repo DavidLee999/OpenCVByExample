7fd6f14f3c5d5b00d54b1e18776dfc3315144d8c
DavidLee999 <MoonLee003@outlook.com>
2017-08-05 11:43:38 +0200

    Ch2 basic opeartions

--- Ch2/sample1.cpp
+++ Ch2/sample1.cpp
@@ -7,24 +7,24 @@ using namespace std;
 #include "opencv2/highgui.hpp"
 using namespace cv;
 
-int main(int argc, const char** argv)
-{
-	Mat color = imread(R"(E:\programmingExercise\OpenCVByExample\Ch1\lena.jpg)");
-	Mat gray = imread(R"(E:\programmingExercise\OpenCVByExample\Ch1\lena.jpg)", 0);
-
-	imwrite("lenaGray.jpg", gray);
-
-	int myRow = color.cols - 1;
-	int myCol = color.rows - 1;
-	Vec3b pixel = color.at<Vec3b>(myRow, myCol);
-
-	cout << "Pixel value (B, G, R): ( " << (int)pixel[0] << ", " << (int)pixel[1] << ", " <<
-		(int)pixel[2] << " )" << endl;
-
-	imshow("Lena BGR", color);
-	imshow("Lena Gray", gray);
-
-	waitKey(0);
-
-	return 0;
-}
+//int main(int argc, const char** argv)
+//{
+//	Mat color = imread(R"(E:\programmingExercise\OpenCVByExample\Ch1\lena.jpg)");
+//	Mat gray = imread(R"(E:\programmingExercise\OpenCVByExample\Ch1\lena.jpg)", 0);
+//
+//	imwrite("lenaGray.jpg", gray);
+//
+//	int myRow = color.cols - 1;
+//	int myCol = color.rows - 1;
+//	Vec3b pixel = color.at<Vec3b>(myRow, myCol);
+//
+//	cout << "Pixel value (B, G, R): ( " << (int)pixel[0] << ", " << (int)pixel[1] << ", " <<
+//		(int)pixel[2] << " )" << endl;
+//
+//	imshow("Lena BGR", color);
+//	imshow("Lena Gray", gray);
+//
+//	waitKey(0);
+//
+//	return 0;
+//}

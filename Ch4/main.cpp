6f7aa85f041cf078aad3fb18b25463026e1f33b8
DavidLee999 <MoonLee003@outlook.com>
2017-08-15 21:31:56 +0200

    Ch4

--- Ch4/main.cpp
+++ Ch4/main.cpp
@@ -14,7 +14,7 @@ const char* keys =
 {
 	"{ help h usage ? | | print this message }"
 	"{ @image | | Image to process }"
-	"{ @operation | | Operation }"
+	"{ @operation | | Operation: hist, equal, lomo, cart }"
 };
 
 void showHisto(const cv::Mat& img)
@@ -60,6 +60,97 @@ void showHisto(const cv::Mat& img)
     }
 	imshow("Histogram", histImage);
 }
+
+void equalizeCallback(const cv::Mat& img)
+{
+	Mat result;
+
+	Mat ycrcb;
+	cvtColor(img, ycrcb, COLOR_BGR2YCrCb);
+
+	vector<Mat> channels;
+	split(ycrcb, channels);
+
+	equalizeHist(channels[0], channels[0]);
+	merge(channels, ycrcb);
+
+	cvtColor(ycrcb, result, COLOR_YCrCb2BGR);
+
+	imshow("Equalized", result);
+}
+
+void lomoCallback(const cv::Mat& img)
+{
+	Mat result;
+
+	const double exponential_e = std::exp(1.0);
+
+	Mat lut(1, 256, CV_8UC1);
+	for (int i = 0; i < 256; ++i)
+	{
+		float x = (float)i / 256.0;
+		lut.at<uchar>(i) = cvRound(256 * (1 / (1 + pow(exponential_e, -((x - 0.5) / 0.1)))));
+	}
+
+	vector<Mat> bgr;
+	split(img, bgr);
+	LUT(bgr[2], lut, bgr[2]);
+
+	merge(bgr, result);
+
+	Mat halo{ img.rows, img.cols, CV_32FC3, Scalar{ 0.3, 0.3, 0.3 } };
+
+	circle(halo, Point{ img.cols / 2, img.rows / 2 }, img.cols / 3, Scalar{ 1, 1, 1 }, -1);
+	blur(halo, halo, Size{ img.cols / 3, img.cols / 3 });
+
+	Mat resultf;
+	result.convertTo(resultf, CV_32FC3);
+
+	multiply(resultf, halo, resultf);
+	resultf.convertTo(result, CV_8UC3);
+
+	imshow("Lomograpy", result);
+}
+
+void cartoonCallback(const cv::Mat& img)
+{
+	Mat imgMedian;
+	medianBlur(img, imgMedian, 7);
+
+	Mat imgCanny;
+	Canny(imgMedian, imgCanny, 50, 150);
+
+	Mat kernel = getStructuringElement(MORPH_RECT, Size{ 2, 2 });
+	dilate(imgCanny, imgCanny, kernel);
+
+	imgCanny = imgCanny / 255;
+	imgCanny = 1 - imgCanny;
+
+	Mat imgCannyf;
+	imgCanny.convertTo(imgCannyf, CV_32FC3);
+
+	blur(imgCannyf, imgCannyf, Size{ 5, 5 });
+
+	Mat imgBF;
+	bilateralFilter(img, imgBF, 9, 150.0, 150.0);
+
+	Mat result = imgBF / 25;
+	result *= 25;
+
+	Mat imgCanny3c;
+	Mat cannyChannels[] = { imgCannyf, imgCannyf, imgCannyf };
+	merge(cannyChannels, 3, imgCanny3c);
+
+	Mat resultf;
+	result.convertTo(resultf, CV_32FC3);
+
+	multiply(resultf, imgCanny3c, resultf);
+
+	resultf.convertTo(result, CV_8UC3);
+
+	imshow("Cartoon", result);
+}
+
 int main(int argc, const char** argv)
 {
     CommandLineParser parser(argc, argv, keys);
@@ -89,10 +180,16 @@ int main(int argc, const char** argv)
     // createButton("Cartonize effect", cartoonCallBack, NULL, QT_PUSH_BUTTON, 0);
 
     imshow("Input", img);
+
 	if (op == "hist")
-	{
 		showHisto(img);
-	}
+	else if (op == "equal")
+		equalizeCallback(img);
+	else if (op == "lomo")
+		lomoCallback(img);
+	else if (op == "cart")
+		cartoonCallback(img);
+
     waitKey(0);
 
     return 0;
